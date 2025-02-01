223f1d7c5844dadd0b605369a6c155fa11f0d1b1
Luna <ratherbelunar@gmail.com>
2025-02-01 12:36:46 -0500

    Tweak cauldron advancement to require placing one above fire. Might need to add an extra condition for lighting fire below cauldron

--- src/main/java/com/bwt/generation/AdvancementsGenerator.java
+++ src/main/java/com/bwt/generation/AdvancementsGenerator.java
@@ -1,28 +1,43 @@
 package com.bwt.generation;
 
 import com.bwt.blocks.BwtBlocks;
+import com.bwt.blocks.abstract_cooking_pot.AbstractCookingPotBlock;
 import com.bwt.entities.BwtEntities;
 import com.bwt.items.BwtItems;
 import com.bwt.utils.Id;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementCriterion;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementFrame;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.advancement.criterion.ItemCriterion;
 import net.minecraft.advancement.criterion.SummonedEntityCriterion;
 import net.minecraft.advancement.criterion.TickCriterion;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.loot.condition.AnyOfLootCondition;
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.condition.LocationCheckLootCondition;
+import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.predicate.BlockPredicate;
+import net.minecraft.predicate.StatePredicate;
 import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.LocationPredicate;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class AdvancementsGenerator extends FabricAdvancementProvider {
     public static final Identifier background = Id.mc("textures/gui/advancements/backgrounds/adventure.png");
@@ -62,11 +77,19 @@ public class AdvancementsGenerator extends FabricAdvancementProvider {
                 .criterion("placed_windmill", SummonedEntityCriterion.Conditions.create(EntityPredicate.Builder.create().type(BwtEntities.windmillEntity)))
                 .build(consumer, "bwt/placed_windmill");
 
-        AdvancementEntry cauldronAdvancement = blockPlacedAdvancement(BwtBlocks.cauldronBlock, "Perpetual Stew", "Place a cauldron over some fire. Baby, you got a stew going!").parent(rootAdvancement).build(consumer, "bwt/placed_cauldron");
+        AdvancementEntry cauldronAdvancement = neighborStateAdvancement(
+                BwtBlocks.cauldronBlock,
+                "Perpetual Stew",
+                "Place a cauldron over some fire. Baby, you got a stew going!",
+                Direction.DOWN,
+                LocationPredicate.Builder.create().block(BlockPredicate.Builder.create().tag(BlockTags.FIRE))
+        ).parent(rootAdvancement).build(consumer, "bwt/placed_cauldron");
         AdvancementEntry lightBlockAdvancement = itemAdvancement(BwtBlocks.lightBlockBlock, "Underground Grow Operation", "Create a light block. To hemp, it might as well be the sun").parent(cauldronAdvancement).build(consumer, "bwt/got_light_block");
         AdvancementEntry dungAdvancement = itemAdvancement(BwtItems.dungItem, "The Stench of Progress", "Feed your pet wolf, and let nature take its course").parent(cauldronAdvancement).build(consumer, "bwt/got_dung");
         AdvancementEntry tannedLeatherAdvancement = itemAdvancement(BwtItems.tannedLeatherItem, "Dung-Tanned", "Tan leather in the cauldron using dung").parent(dungAdvancement).build(consumer, "bwt/got_tanned_leather");
         AdvancementEntry sawAdvancement = blockPlacedAdvancement(BwtBlocks.sawBlock, "DIY Carpentry", "Build and power a saw in pursuit of finer wood pieces").parent(tannedLeatherAdvancement).build(consumer, "bwt/placed_saw");
+
+
     }
 
     public Advancement.Builder itemAdvancement(ItemConvertible item, String title, String description) {
@@ -85,7 +108,7 @@ public class AdvancementsGenerator extends FabricAdvancementProvider {
                 .criterion("got_" + Registries.ITEM.getId(item.asItem()).getPath(), InventoryChangedCriterion.Conditions.items(item));
     }
 
-    public Advancement.Builder blockPlacedAdvancement(Block block, String title, String description) {
+    public Advancement.Builder blockPlacedAdvancement(Block block, String title, String description, AdvancementCriterion<?> criterion) {
         return Advancement.Builder.create()
                 .display(
                         block.asItem(), // The display icon
@@ -98,6 +121,23 @@ public class AdvancementsGenerator extends FabricAdvancementProvider {
                         false // Hidden in the advancement tab
                 )
                 // The first string used in criterion is the name referenced by other advancements when they want to have 'requirements'
-                .criterion("place_" + Registries.BLOCK.getId(block).getPath(), ItemCriterion.Conditions.createPlacedBlock(block));
+                .criterion("place_" + Registries.BLOCK.getId(block).getPath(), criterion);
+    }
+
+    public Advancement.Builder blockPlacedAdvancement(Block block, String title, String description) {
+        return blockPlacedAdvancement(block, title, description, ItemCriterion.Conditions.createPlacedBlock(block));
+    }
+
+    public Advancement.Builder neighborStateAdvancement(
+            Block block,
+            String title,
+            String description,
+            Direction direction,
+            LocationPredicate.Builder neighborStatePredicate
+    ) {
+        LootCondition.Builder thisCondition = BlockStatePropertyLootCondition.builder(block);
+        LootCondition.Builder neighborCondition = LocationCheckLootCondition.builder(neighborStatePredicate, new BlockPos(direction.getVector()));
+        AdvancementCriterion<ItemCriterion.Conditions> criterion = ItemCriterion.Conditions.createPlacedBlock(thisCondition, neighborCondition);
+        return blockPlacedAdvancement(block, title, description, criterion);
     }
 }
