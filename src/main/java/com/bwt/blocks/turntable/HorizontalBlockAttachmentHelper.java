0f0a0b92be6d4ca92a1d2cf8939f75ad3dd012ad
Luna <ratherbelunar@gmail.com>
2025-11-07 18:23:32 -0500

    Disallow vines being rotated as a horizontal attachment on the turntable

--- src/main/java/com/bwt/blocks/turntable/HorizontalBlockAttachmentHelper.java
+++ src/main/java/com/bwt/blocks/turntable/HorizontalBlockAttachmentHelper.java
@@ -67,7 +67,6 @@ public interface HorizontalBlockAttachmentHelper {
         register(WallBannerBlock.class, horizontalFacingBlockPredicate);
         register(TripwireHookBlock.class, horizontalFacingBlockPredicate);
         register(LadderBlock.class, horizontalFacingBlockPredicate);
-        register(VineBlock.class, connectingBlockPredicate);
         register(GlowLichenBlock.class, connectingBlockPredicate);
         register(AmethystClusterBlock.class, facingBlockPredicate);
         register(BellBlock.class, bellPredicate);
