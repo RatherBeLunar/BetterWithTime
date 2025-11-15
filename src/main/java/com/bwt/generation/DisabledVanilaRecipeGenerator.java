68b0ae48deda2685e1838584eb67e6e2cbc05c85
Luna <ratherbelunar@gmail.com>
2025-11-15 10:41:39 -0500

    Fix vanilla sugar cane recipe disabling

--- src/main/java/com/bwt/generation/DisabledVanilaRecipeGenerator.java
+++ src/main/java/com/bwt/generation/DisabledVanilaRecipeGenerator.java
@@ -45,7 +45,7 @@ public class DisabledVanilaRecipeGenerator extends FabricRecipeProvider {
         disableVanilla(Items.FLOWER_POT, exporter);
         disableVanilla(Items.BONE_MEAL, exporter);
         disableVanilla(Items.BREAD, exporter);
-        disableVanilla(Items.SUGAR, exporter);
+        disableVanilla(Items.SUGAR, "_from_sugar_cane", exporter);
         disableVanilla(Items.CAKE, exporter);
         disableVanilla(Items.COOKIE, exporter);
 
